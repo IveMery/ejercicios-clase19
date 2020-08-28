d36b5c84d2d4a371dfdf3641465d820877165d1d
IvetteMery <ivette.mery@gmail.com>
2020-08-27 20:53:26 -0400

    1

---
+++ ejercicio1/main.js
@@ -0,0 +1,25 @@
+ const cancionUno=prompt(`Agregar la primera cancion`)
+  const cancionDos=prompt(`Agregar segunda cancion`)
+  const cancionTres=prompt(`Agregar tercera cancion`) 
+  const cancionCuatro =prompt(`Agregar cuarta cancion`) 
+  const cancionCinco =prompt(`Agregar quinta cancion`)
+
+ const cancion1= document.getElementById(`cancionUno`)
+  console.log(cancionUno)
+ cancion1.textContent = cancionUno
+
+ const cancion2 = document.getElementById(`cancionDos`)
+   console.log(cancionDos)
+  cancion2.textContent = cancionDos
+
+  const cancion3 = document.getElementById(`cancionTres`)
+   console.log(cancionTres)
+  cancion3.textContent = cancionTres
+
+  const cancion4 = document.getElementById(`cancionCuatro`)
+   console.log(cancionCuatro)
+  cancion4.textContent = cancionCuatro
+
+  const cancion5 = document.getElementById(`cancionCinco`)
+   console.log(cancionCinco)
+ cancion5.textContent = cancionCinco
---
+++ ejercicio4/README.md
@@ -0,0 +1,10 @@
+# ejercicio4-clase19
+
+paleta
+Crear un documento html que tenga 6 botones con
+ un color de fondo distinto, y un div cuadrado grande. 
+ Cuando se clickea un botón, el div debe cambiar su color
+  de fondo
+ al mismo color de fondo que el del botón clickeado.
+ 
+[Ver ejercicio](https://ecstatic-cori-32bda8.netlify.app/)
